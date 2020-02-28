279e97d754add5642eafcf5183637a81aecfc78d
Vitalik Kim <kimv1086@gmail.com>
2020-02-28 11:52:42 +0600

    fix

--- api/server.js
+++ api/server.js
@@ -4,15 +4,24 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 
-const port = 8000;
+const port = 9000;
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cors());
 
-mongoose.connect('mongodb://localhost/shortLink', {useNewUrlParser: true}).then(() => {
+const start = async () => {
+  await mongoose.connect('mongodb://localhost/shortLink', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
   app.use('/link', link);
 
   app.listen(port, () => {
     console.log(`Server started on ${port} port`);
   });
+};
+
+start().catch(err => {
+  console.error(err);
 });
